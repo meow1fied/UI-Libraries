296f085661112dda3ec00488cdec31b0c7cb19e5
meows <149817820+meow1fied@users.noreply.github.com>
2024-09-12 21:05:25 +0100

    Update source.lua

--- Obelus/source.lua
+++ Obelus/source.lua
@@ -85,7 +85,7 @@ do
 			BorderSizePixel = 1,
 			Parent = screen,
 			Position = UDim2.new(0.5, 0, 0.5, 0),
-			Size = UDim2.new(0, 350, 0, 350)
+			Size = UDim2.new(0, 500, 0, 550)
 		}})
 		--
 		local frame = utility:Create({Type = "Frame", Properties = {
